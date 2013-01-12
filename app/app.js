6c0b7090a8beebc1e6817a4f26242a936a5ff9fa
sambillingham <sambillie.sb@gmail.com>
2013-01-12 19:18:33 +0000

    still done nothing

--- app/app.js
+++ app/app.js
@@ -1,17 +1,42 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var connections = require('connections');
+
+connections = new connections();
+
+connections.hello();
+
+// App config
 
 app.configure(function() {
 
+	// Path to app views
+	app.set('views', __dirname + '/views');
+
+	app.set('view engine', 'jade');
+
+	// Path to public
 	app.use(express.static(__dirname + '/public'));
 
 });
 
+// Home
 app.get('/', function(req, res) {
-	res.sendfile("index.html");
+	res.sendfile('index.html');
 });
 
-// Start server
+// Room select
+app.get('/home', function(req, res) {
+	res.render('home');
+});
+
+// Single room
+app.get('/room/:id', function(req, res) {
+	res.render('room', {
+		room: req.params
+	});
+});
+
 app.listen(port);
 console.log('Listening on port 3000');
