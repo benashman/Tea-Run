48f65303722c63d93e77f756c27f039918681441
Ben Ashman <ben.ashman@me.com>
2013-01-12 15:47:45 +0000

    Added basic routes. Begin OAuth integration

--- app/app.js
+++ app/app.js
@@ -2,16 +2,40 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+// CONFIG
+
 app.configure(function() {
 
+	// Path to app views
+	app.set('views', __dirname + '/views');
+
+	app.set('view engine', 'jade');
+
+	// Path to public
 	app.use(express.static(__dirname + '/public'));
 
 });
 
+// ROUTES
+
+// Home
 app.get('/', function(req, res) {
-	res.sendfile("index.html");
+	res.sendfile('index.html');
 });
 
-// Start server
+// Room select
+app.get('/rooms', function(req, res) {
+	res.render('rooms');
+});
+
+// Single room
+app.get('/room/:id', function(req, res) {
+	res.render('room', {
+		room: req.params
+	});
+});
+
+// START
+
 app.listen(port);
 console.log('Listening on port 3000');
