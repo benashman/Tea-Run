fb8b589465061f0c92cda7d7a12b96a3542211ab
Saul Hardman <hello@iamsaul.co.uk>
2013-01-12 14:53:29 +0000

    Local commit before Ben's structure overhaul

--- node/app.js
+++ node/app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var _connections = require('connections');
+var connections = new _connections();
+
+connections.hello();
 
 app.get('/hello', function(req, res) {
 });
